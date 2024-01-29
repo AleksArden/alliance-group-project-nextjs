a1e9a7c27c1493c809d5ffa44f04b3f322da28c4
Oleksii Chubenko <i1723@i.ua>
2024-01-29 15:52:02 +0000

    added tablet size to homeProducts and homeServices

--- src/components/clientHomePage/ClientHomePage.tsx
+++ src/components/clientHomePage/ClientHomePage.tsx
@@ -1,7 +1,13 @@
 'use client';
 
 import styles from './ClientHomePage.module.scss';
-import { HomePageType, IntroType } from 'types/dataTypeForFirebase';
+import {
+  HomePageType,
+  HomeProductsType,
+  HomeServicesType,
+  IntroType,
+  ProductServiceType,
+} from 'types/dataTypeForFirebase';
 import BackgroundImage from 'components/backgroundImage/BackgroundImage';
 import AnimationIntro from 'components/animationIntro/AnimationIntro';
 import { Lang } from 'types/otherType';
@@ -9,17 +15,33 @@ import ContentHeroHome from 'components/contentHeroHome/ContentHeroHome';
 import AnimationHeroHome from 'components/animationHeroHome/AnimationHeroHome';
 
 import { useIsWideScreen } from 'hooks/useIsWideScreen';
+import ContentHeroOtherPages from 'components/contentHeroOtherPages/ContentHeroOtherPages';
+import AnimationHeroOtherPages from 'components/anomationHeroOtherPages/AnimationHeroOtherPages';
+import SliderHomeProducts from 'components/sliderHomeProducts/SliderHomeProducts';
+import SliderHomeServices from 'components/sliderHomeServices/SliderHomeServices';
 
 interface IProps {
   props: {
     dataHero: HomePageType | undefined;
     dataIntro: IntroType | undefined;
+    dataHomeProducts: HomeProductsType | undefined;
+    listAllProducts: ProductServiceType[] | undefined;
+    dataHomeServices: HomeServicesType | undefined;
+    listAllServices: ProductServiceType[] | undefined;
     locale: string;
   };
 }
 
 const ClientHomePage = ({ props }: IProps) => {
-  const { dataHero, dataIntro, locale } = props;
+  const {
+    dataHero,
+    dataIntro,
+    dataHomeProducts,
+    listAllProducts,
+    dataHomeServices,
+    listAllServices,
+    locale,
+  } = props;
   const [isDesktopScreen, isTabletScreen] = useIsWideScreen();
 
   return (
@@ -76,6 +98,176 @@ const ClientHomePage = ({ props }: IProps) => {
               <BackgroundImage imageUrl={dataIntro.backgroundImageDesktop} />
             )}
             <AnimationIntro text={dataIntro.text} sign={dataIntro.sign} />
+          </>
+        )}
+      </section>
+
+      <section className={styles.products}>
+        {dataHomeProducts && (
+          <>
+            {/* <BackgroundImage imageUrl={dataHomeProducts.backgroundImageMobile} /> */}
+
+            {isTabletScreen && (
+              <BackgroundImage
+                imageUrl={dataHomeProducts.backgroundImageTablet}
+              />
+            )}
+
+            {isDesktopScreen && (
+              <BackgroundImage
+                imageUrl={dataHomeProducts.backgroundImageDesktop}
+              />
+            )}
+            {locale === Lang.UK && (
+              <>
+                <ContentHeroOtherPages
+                  title={dataHomeProducts.titleUK}
+                  classTitleHome={true}
+                />
+
+                {isDesktopScreen && (
+                  <AnimationHeroOtherPages
+                    title={dataHomeProducts.titleUK}
+                    top="600px"
+                  />
+                )}
+                {isTabletScreen && (
+                  <AnimationHeroOtherPages
+                    title={dataHomeProducts.titleUK}
+                    top="630px"
+                  />
+                )}
+              </>
+            )}
+            {locale === Lang.EN && (
+              <>
+                <ContentHeroOtherPages
+                  title={dataHomeProducts.titleEN}
+                  classTitleHome={true}
+                />
+
+                {isDesktopScreen && (
+                  <AnimationHeroOtherPages
+                    title={dataHomeProducts.titleEN}
+                    top="600px"
+                  />
+                )}
+                {isTabletScreen && (
+                  <AnimationHeroOtherPages
+                    title={dataHomeProducts.titleEN}
+                    top="630px"
+                  />
+                )}
+              </>
+            )}
+            {locale === Lang.TR && (
+              <>
+                <ContentHeroOtherPages
+                  title={dataHomeProducts.titleTR}
+                  classTitleHome={true}
+                />
+
+                {isDesktopScreen && (
+                  <AnimationHeroOtherPages
+                    title={dataHomeProducts.titleTR}
+                    top="600px"
+                  />
+                )}
+                {isTabletScreen && (
+                  <AnimationHeroOtherPages
+                    title={dataHomeProducts.titleTR}
+                    top="630px"
+                  />
+                )}
+              </>
+            )}
+            {listAllProducts && (
+              <SliderHomeProducts products={listAllProducts} locale={locale} />
+            )}
+          </>
+        )}
+      </section>
+      <section className={styles.services}>
+        {dataHomeServices && (
+          <>
+            {/* <BackgroundImage imageUrl={dataHomeServices.backgroundImageMobile} /> */}
+
+            {isTabletScreen && (
+              <BackgroundImage
+                imageUrl={dataHomeServices.backgroundImageTablet}
+              />
+            )}
+
+            {isDesktopScreen && (
+              <BackgroundImage
+                imageUrl={dataHomeServices.backgroundImageDesktop}
+              />
+            )}
+
+            {locale === Lang.UK && (
+              <>
+                <ContentHeroOtherPages
+                  title={dataHomeServices.titleUK}
+                  classTitleHome={true}
+                />
+                {isDesktopScreen && (
+                  <AnimationHeroOtherPages
+                    title={dataHomeServices.titleUK}
+                    top="600px"
+                  />
+                )}
+                {isTabletScreen && (
+                  <AnimationHeroOtherPages
+                    title={dataHomeServices.titleUK}
+                    top="630px"
+                  />
+                )}
+              </>
+            )}
+            {locale === Lang.EN && (
+              <>
+                <ContentHeroOtherPages
+                  title={dataHomeServices.titleEN}
+                  classTitleHome={true}
+                />
+                {isDesktopScreen && (
+                  <AnimationHeroOtherPages
+                    title={dataHomeServices.titleEN}
+                    top="600px"
+                  />
+                )}
+                {isTabletScreen && (
+                  <AnimationHeroOtherPages
+                    title={dataHomeServices.titleEN}
+                    top="630px"
+                  />
+                )}
+              </>
+            )}
+            {locale === Lang.TR && (
+              <>
+                <ContentHeroOtherPages
+                  title={dataHomeServices.titleTR}
+                  classTitleHome={true}
+                />
+                {isDesktopScreen && (
+                  <AnimationHeroOtherPages
+                    title={dataHomeServices.titleTR}
+                    top="600px"
+                  />
+                )}
+                {isTabletScreen && (
+                  <AnimationHeroOtherPages
+                    title={dataHomeServices.titleTR}
+                    top="630px"
+                  />
+                )}
+              </>
+            )}
+
+            {listAllServices && (
+              <SliderHomeServices services={listAllServices} locale={locale} />
+            )}
           </>
         )}
       </section>
