46df2cda49642344bf378d5de1a6f576b34bdc3a
Oleksii Chubenko <i1723@i.ua>
2024-01-31 22:23:04 +0000

    fixed error

--- src/components/navBar/NavBarLogo.tsx
+++ src/components/navBar/NavBarLogo.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-import styles from 'components/navBar/NavBar.module.scss';
+import styles from 'components/navBar/NavBarLogo.module.scss';
 
 const NavBarLogo = () => {
   const pathname = usePathname();
