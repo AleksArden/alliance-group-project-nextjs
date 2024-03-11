40ab96da6d757c998c4120624c08a93a3c99cf49
Oleksii Chubenko <i1723@i.ua>
2024-03-11 12:02:50 +0000

    moved getMediaFromInstagram into InstagramGallery

--- src/components/instagramGallery/InstagramGallery.tsx
+++ src/components/instagramGallery/InstagramGallery.tsx
@@ -1,23 +1,35 @@
-import { InstagramPostType } from 'types/otherType';
+import { InstagramPostType, InstagramResponse } from 'types/otherType';
 import styles from './InstagramGallery.module.scss';
 import InstaFeed from './instaFeed/InstaFeed';
 import { Suspense } from 'react';
 import Loading from 'app/[locale]/(marketing)/loading';
 
+async function getMediaFromInstagram(): Promise<InstagramResponse> {
+  const fieldsName = 'id,caption,media_type,media_url';
+  const url = `https://graph.instagram.com/me/media?fields=${fieldsName}&limit=30&access_token=${process.env.INSTAGRAM_KEY}`;
+  const res = await fetch(url, {
+    next: { revalidate: 21600 },
+  });
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  return res.json() as Promise<InstagramResponse>;
+}
+
 type IProps = {
   locale: string;
-  feeds: InstagramPostType[];
 };
 
-const InstagramGallery = ({ locale, feeds }: IProps) => {
-  // console.log(feeds);
+const InstagramGallery = async ({ locale }: IProps) => {
+  const feeds = await getMediaFromInstagram();
+  // console.log('instagram', feeds);
 
   return (
     <section className={styles.container}>
       <Suspense fallback={<Loading />}>
         <ul className={styles.list}>
           {feeds &&
-            feeds.map((feed: InstagramPostType) => (
+            feeds.data.map((feed: InstagramPostType) => (
               <InstaFeed key={feed.id} feed={feed} locale={locale} />
             ))}
         </ul>
