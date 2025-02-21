b820d0f1320e735f3e655f98d923fb12eec73ced
Aleks_Arden <aleks_arden@AleksArden.local>
2025-02-21 16:37:52 +0000

    added instagram route interceptor

---
+++ src/app/api/auth/callback/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const code = searchParams.get('code');
+
+  if (!code) {
+    return NextResponse.json(
+      { error: 'Authorization code not found' },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const response = await fetch(
+      'https://api.instagram.com/oauth/access_token',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+          client_id: process.env.NEXT_PUBLIC_INSTAGRAM_CLIENT_ID!,
+          client_secret: process.env.INSTAGRAM_CLIENT_SECRET!,
+          grant_type: 'authorization_code',
+          redirect_uri: process.env.NEXT_PUBLIC_REDIRECT_URI!,
+          code,
+        }),
+      }
+    );
+
+    const data = await response.json();
+
+    console.log('Access Token>>>>>>>>>>:', data.access_token);
+
+    return NextResponse.json({ access_token: data.access_token });
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Failed to fetch access token' },
+      { status: 500 }
+    );
+  }
+}
--- src/components/instagramGallery/InstagramGallery.tsx
+++ src/components/instagramGallery/InstagramGallery.tsx
@@ -4,25 +4,30 @@ import InstaFeed from './instaFeed/InstaFeed';
 import { Suspense } from 'react';
 import Loading from 'app/[locale]/(marketing)/loading';
 
-// async function getMediaFromInstagram(): Promise<InstagramResponse> {
-//   const fieldsName = 'id,caption,media_type,media_url';
-//   const url = `https://graph.instagram.com/me/media?fields=${fieldsName}&limit=30&access_token=${process.env.INSTAGRAM_KEY}`;
-//   const res = await fetch(url, {
-//     next: { revalidate: 86400 },
-//   });
-//   if (!res.ok) {
-//     throw new Error('Failed to fetch data');
-//   }
-//   return res.json() as Promise<InstagramResponse>;
-// }
+async function getMediaFromInstagram(): Promise<InstagramResponse> {
+  const fieldsName = 'id,caption,media_type,media_url';
+  const url = `https://graph.facebook.com/v22.0/100984171886281
+?fields=instagram_business_account&access_token=${process.env.INSTAGRAM_KEY}`;
+  //   const url = `https://graph.instagram.com/100984171886281
+  // /media?fields=${fieldsName}&limit=30&access_token=${process.env.INSTAGRAM_KEY}`;
+  const res = await fetch(url, {
+    next: { revalidate: 86400 },
+  });
+  if (!res.ok) {
+    const errorText = await res.text();
+    console.error(`Error fetching Instagram data: ${errorText}`);
+    throw new Error('Failed to fetch data');
+  }
+  return res.json() as Promise<InstagramResponse>;
+}
 
 type IProps = {
   locale: string;
 };
 
 const InstagramGallery = async ({ locale }: IProps) => {
-  // const feeds = await getMediaFromInstagram();
-  // console.log('instagram', feeds);
+  const feeds = await getMediaFromInstagram();
+  console.log('instagram>>>>>>>>>>>', feeds);
 
   return (
     <section className={styles.container}>
